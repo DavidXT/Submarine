3ee781acfd32801976e6f4550b781ead7038cb76
DavidXT <David.xt.nguyen@gmail.com>
2021-04-20 17:40:26 +0200

    Start of player movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private Vector3 mouvement;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouvement = Vector3.zero;
+        mouvement.x = Input.GetAxisRaw("Horizontal");
+        mouvement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        if (mouvement != Vector3.zero)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        _rb.MovePosition(transform.position + mouvement * speed * Time.deltaTime); //Déplacement du joueur
+    }
+}
