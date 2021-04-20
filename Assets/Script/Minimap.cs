c71c78461ddb701c8fbf81e5fe8cf80f8439a686
DavidXT <David.xt.nguyen@gmail.com>
2021-04-20 20:17:39 +0200

    Start minimap

--- Assets/Script/Minimap.cs
+++ Assets/Script/Minimap.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Minimap : MonoBehaviour
 {
+    public float range;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,16 @@ public class Minimap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject[] ennemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        foreach (GameObject enemy in ennemies)
+        {
+            float distanceToEnemy = Vector3.Distance(player.transform.position, enemy.transform.position);
+            if (distanceToEnemy <= range)
+            {
+                //Draw l'ennemy
+            }
+        }
     }
 }
