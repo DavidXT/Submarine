aa58f4eccb2f4a061a5a497ffb8f21aedd196190
David Nguyen <david.xt.nguyen@gmail.com>
2021-04-21 15:31:19 +0200

    limit vitesse

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,11 +9,13 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private float acc;
     private Vector3 mouvementSubmarine;
+    private float limit;
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         acc = speed;
+        limit = acc;
     }
 
     // Update is called once per frame
@@ -22,28 +24,49 @@ public class PlayerController : MonoBehaviour
         mouvement = Vector3.zero;
         mouvement.x = Input.GetAxisRaw("Horizontal");
         mouvement.y = Input.GetAxisRaw("Vertical");
+
+        /* Déplacement */
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             mouvementSubmarine.x = mouvement.x;
-            acc += 1;
+            limit += 1;
+            while (acc < limit)
+            {
+                acc += Time.deltaTime;
+            }
         }        
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            acc -= 1;
-            if(acc < 0)
+            limit = limit - 1;
+            while (acc > limit)
+            {
+                acc -= Time.deltaTime;
+            }
+            if (acc < 0)
             {
                 mouvementSubmarine.x = -mouvement.x;
             }
-        }        
+        }
+
+        /*Rotation*/
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * 10, Space.World);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, -1) * Time.deltaTime * 10, Space.World);
+        }
+
         if(acc == 0)
         {
             mouvementSubmarine = mouvement;
         }
-        
     }
 
     private void FixedUpdate()
     {
-        _rb.MovePosition(transform.position + mouvementSubmarine * acc * Time.deltaTime); //Déplacement du joueur
+        _rb.MovePosition(transform.position + transform.right * acc * Time.deltaTime); //Déplacement du joueur
+
     }
 }
