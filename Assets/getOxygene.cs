17d8707322807cc2508f2192f7016e372d346846
David Nguyen <david.xt.nguyen@gmail.com>
2021-04-22 15:06:35 +0200

    Alarm

--- Assets/getOxygene.cs
+++ Assets/getOxygene.cs
@@ -9,11 +9,15 @@ public class getOxygene : MonoBehaviour
     private float arrowRotation;
     PlayerController playerscript;
     public Image oxygeneImg;
+    public Image alarm;
+    public float ratioAlarm;
+    public float compteurAlarm;
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player");
         playerscript = target.GetComponent<PlayerController>();
+        compteurAlarm = 0;
     }
 
     // Update is called once per frame
@@ -21,5 +25,23 @@ public class getOxygene : MonoBehaviour
     {
         float FilledOxygene = playerscript.oxygene / playerscript.maxOxygene;
         oxygeneImg.fillAmount = FilledOxygene;
+        if (playerscript.oxygene <= playerscript.maxOxygene / 10)
+        {
+            oxygeneImg.color = new Color32(255, 14, 0, 255);
+            compteurAlarm += Time.deltaTime;
+            float timerAlarm = Mathf.Sin(ratioAlarm * 2 * 3.14f * compteurAlarm);
+            if (timerAlarm <= 0)
+            {
+                alarm.color = new Color32(0, 0, 0, 255);
+            }
+            else
+            {
+                alarm.color = new Color32(255, 14, 0, 255);
+            }
+        }
+        else{
+            oxygeneImg.color = new Color32(49, 255, 0, 255);
+            alarm.color = new Color32(0, 0, 0, 255);
+        }
     }
 }
